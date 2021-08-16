9b2a102bd67845cd33ec54081e325b5bb481ef48
coffmanjrp <coffmanjrp@gmail.com>
2021-08-16 15:33:59 -0700

    HTML and styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,20 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <div className="container">
+        <div className="split left">
+          <h1>PlayStation 5</h1>
+          <a href="#!" className="btn">
+            Buy Now
+          </a>
+        </div>
+        <div className="split right">
+          <h1>XBox Series X</h1>
+          <a href="#!" className="btn">
+            Buy Now
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
